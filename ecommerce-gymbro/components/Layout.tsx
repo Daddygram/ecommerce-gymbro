346106c75ac2ee8f2ece91a3bcf7d70d901f387e
Joseph Moninyo <tsotne.s.2004@gmail.com>
2023-12-10 14:16:51 +0400

    added-footer

--- ecommerce-gymbro/components/Layout.tsx
+++ ecommerce-gymbro/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
-    <div className='layout'>
+    <div>
         <header>
             <Navbar />
         </header>
